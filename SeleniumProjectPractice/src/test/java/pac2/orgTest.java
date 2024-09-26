c8158782fd8636f7402b24c4b34671719b5c5f5e
OM <OM@DESKTOP-0BC3S1D.mshome.net>
2024-09-26 18:20:23 +0530

    framework Move to github

---
+++ SeleniumProjectPractice/src/test/java/pac2/orgTest.java
@@ -0,0 +1,20 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	
+		@Test
+		public void createorgTest() {
+			System.out.println("Execute createorgTest ");
+			
+		}
+		@Test
+		public void createOrgTest() {
+			System.out.println("Execute createOrgTest ");
+			
+		}
+
+	}
+
+
